8e2b3155f91f3ef2747e89e835a0e87dc187ce15
ambareesha.av <ambareesha.av@aurut.com>
2023-07-09 17:07:58 +0530

    chore: add accounts table, schema, context

---
+++ lib/shotodo/accounts/account.ex
@@ -0,0 +1,21 @@
+defmodule Shotodo.Accounts.Account do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "accounts" do
+    field :email, :string
+    field :password, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(account, attrs) do
+    account
+    |> cast(attrs, [:email, :password])
+    |> validate_required([:email, :password])
+    |> unique_constraint(:email)
+  end
+end
--- lib/shotodo_web/router.ex
+++ lib/shotodo_web/router.ex
@@ -2,28 +2,32 @@ defmodule ShotodoWeb.Router do
   use ShotodoWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_live_flash
-    plug :put_root_layout, html: {ShotodoWeb.Layouts, :root}
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html", "json"])
+    plug(:fetch_session)
+    plug(:fetch_live_flash)
+    plug(:put_root_layout, html: {ShotodoWeb.Layouts, :root})
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", ShotodoWeb do
-    pipe_through :browser
+    pipe_through(:browser)
 
-    get "/", PageController, :home
+    get("/", PageController, :home)
   end
 
   # Other scopes may use custom stacks.
-  # scope "/api", ShotodoWeb do
-  #   pipe_through :api
-  # end
+  scope "/api", ShotodoWeb do
+    pipe_through(:api)
+
+    scope "/v1" do
+      resources("/accounts", AccountController, except: [:new, :edit])
+    end
+  end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
   if Application.compile_env(:shotodo, :dev_routes) do
@@ -35,10 +39,10 @@ defmodule ShotodoWeb.Router do
     import Phoenix.LiveDashboard.Router
 
     scope "/dev" do
-      pipe_through :browser
+      pipe_through(:browser)
 
-      live_dashboard "/dashboard", metrics: ShotodoWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
+      live_dashboard("/dashboard", metrics: ShotodoWeb.Telemetry)
+      forward("/mailbox", Plug.Swoosh.MailboxPreview)
     end
   end
 end
